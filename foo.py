a2c42f2a669392241541054cd65630b53b23b598
Christian <bouwensec@gmail.com>
2018-04-10 11:46:51 -0400

    Fixed foo.py indentation

--- foo.py
+++ foo.py
@@ -1,12 +1,12 @@
 """File with a bunch of cool stuff in it."""
 
 def func1 (arg1, arg2):
-	"""<Insert description of function here>
+    """<Insert description of function here>
     @param arg1: <Insert description here>
     @type arg1: <Insert type here>
     @param arg2: <Insert description here>
     @type arg2: <Insert type here>
     """
-	return arg1 + arg2
+    return arg1 + arg2
 
 print (func1 (1, 2))
